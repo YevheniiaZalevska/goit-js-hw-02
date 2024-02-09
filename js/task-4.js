71c36c274223b19b53dd849d1f85030d8253d53f
YevheniiaZalevska <yevhenia05@gmail.com>
2024-02-09 11:42:45 +0100

    54678

--- js/task-4.js
+++ js/task-4.js
@@ -20,9 +20,11 @@ function getShippingCost(country) {
     return message;
 }
 
-// console.log(getShippingCost("Australia")); 
+// console.log(getShippingCost("Australia"));
 // console.log(getShippingCost("Germany"));
-// console.log(getShippingCost("China")); 
-// console.log(getShippingCost("Chile")); 
-// console.log(getShippingCost("Jamaica")); 
-// console.log(getShippingCost("Sweden")); 
+// console.log(getShippingCost("China"));
+// console.log(getShippingCost("Chile"));
+// console.log(getShippingCost("Jamaica"));
+// console.log(getShippingCost("Sweden"));
+
+
